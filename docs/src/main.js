a1d40f46c7a0f1caf7732f9d5110557602f23c29
Naveed ul Hassan Malik <naveed@appmakers.pk>
2020-09-09 16:56:38 +0500

    fixed: added bootstrap

--- docs/src/main.js
+++ docs/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {AppMakersVue} from "appmakers-vue";
+
+Vue.use(AppMakersVue);
 
 Vue.config.productionTip = false
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import {FilterRegistrar} from "./filter-registrar";
 import './utilities/apmFunctions';
 import './styles/appmakers.scss';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import BootstrapVue from "bootstrap-vue";
+
 export const AppMakersVue = {
     install(Vue, options) {
+        Vue.use(BootstrapVue);
         ComponentRegistrar.register(Vue);
         DirectiveRegistrar.register(Vue);
         FilterRegistrar.register(Vue);
