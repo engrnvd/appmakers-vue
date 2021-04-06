ee5c8e512fd02408343f084c57331d01aa67b629
Naveed ul Hassan Malik <naveed@appmakers.pk>
2021-04-06 16:46:15 +0500

    fixed: apm remote select

--- components/ApmRemoteSelect.vue
+++ components/ApmRemoteSelect.vue
@@ -1,44 +1,101 @@
 <template>
-    <div>
-        <b-form-select
-            ref="monthPicker"
-            v-model="selected" @input="$emit('input', selected)"
-            :placeholder="placeholder" :options="options" selected>
-        </b-form-select>
-    </div>
-
+    <v-select
+        class="form-control"
+        :options="options"
+        :label="labelField"
+        :get-option-label="getOptionLabel"
+        :reduce="option => option[valueField]"
+        :value="value"
+        @input="onInput"
+        @search="fetchOptions">
+        <template slot="no-options">{{ placeholder }}</template>
+    </v-select>
 </template>
 
 <script>
-    import {http} from "../services/http-service";
+import {Resource} from "@/appmakers-vue/services/http-resource-service";
 
-    export default {
-        name: "ApmRemoteSelect",
-        props:{
-            url:'',
-            ddValueField:null,
-            ddTextField:null,
-            placeholder:null
+export default {
+    name: "ApmRemoteSelect",
+    props: {
+        url: String,
+        value: String,
+        valueField: {
+            type: String,
+            default: '_id'
         },
-        data() {
-            return {
-                selected: null,
-                options: [],
-
+        labelField: String,
+        getOptionLabel: {
+            type: Function,
+            default(option) {
+                if (typeof option === 'object') {
+                    if (!option.hasOwnProperty(this.label)) {
+                        return console.warn(
+                            `[vue-select warn]: Label key "option.${this.label}" does not` +
+                            ` exist in options object ${JSON.stringify(option)}.\n` +
+                            'https://vue-select.org/api/props.html#getoptionlabel'
+                        )
+                    }
+                    return option[this.label]
+                }
+                return option;
             }
         },
-        mounted() {
-            http.get(this.url).then(response => {
-                for (let user of response.data) {
-                    if (this.ddValueField)
-                        this.options.push({value: user[this.ddValueField], text: user[this.ddTextField]});
-                    else {
-                        this.options.push({value: user, text: user[this.ddTextField]})
-                    }
-                }
+        minSearchLength: {
+            type: Number,
+            default: 2
+        },
+        dataProp: {
+            type: String,
+            default: ''
+        },
+        placeholder: {
+            type: String,
+            default: 'Type to search...'
+        },
+    },
+    data() {
+        return {
+            options: [],
+            req: new Resource(this.url, 'get', {
+                delay: 300,
+                delayFirstRequest: true,
+            })
+        }
+    },
+    methods: {
+        fetchOptions(searchValue, loading) {
+            if (!searchValue || searchValue.length < this.minSearchLength) {
+                return;
+            }
+
+            loading(true);
+
+            this.req.url = this.url + '?search=' + searchValue;
+            this.req.send({
+                onSuccess: (res) => {
+                    this.options = this.dataProp ? res.data[this.dataProp] : res.data;
+                },
+                afterRequest: () => {
+                    loading(false);
+                },
             });
         },
+        onInput(val) {
+            this.$emit('input', val);
+        },
+    },
+    updated() {
+        if (this.value && !this.options.length) {
+            this.req.url = `${this.url}?${this.valueField}=${this.value}`;
+            this.req.send({
+                onSuccess: (res) => {
+                    this.options = this.dataProp ? res.data[this.dataProp] : res.data;
+                },
+            });
+        }
     }
+}
 </script>
 
 <style scoped>
